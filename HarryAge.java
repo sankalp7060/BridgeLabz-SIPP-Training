c7f4217d98deca1358714a3680bfabb29a4019ce
Sankalp <129958359+sankalp7060@users.noreply.github.com>
2025-06-12 12:13:50 +0530

    Delete HarryAge.java
    Deleting some wrongly uploaded files

--- HarryAge.java
+++
@@ -1,8 +0,0 @@
-public class HarryAge{
-  public static void main(String[] args){
-    int birth_year=2000;
-    int current_year=2024;
-    int harry_age=current_yearbirth_year;
-    System.out.print(" Harry's age in 2024 is "+harry_age);
-  }
-}
