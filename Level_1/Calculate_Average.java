0e3357786eebd9fd86e859f0839e880746d2c8c5
Sankalp <129958359+sankalp7060@users.noreply.github.com>
2025-06-13 08:56:25 +0530

    Delete Level_1 directory
    Removed unused directory 'Level_1' to clean up project

--- Level_1/Calculate_Average.java
+++
@@ -1,9 +0,0 @@
-public class Calculate_Average{
-  public static void main(String[] args){
-    int maths_marks=94;
-    int physics_marks=95;
-    int chemistry_marks=96;
-    int averageOfMarks=(maths_marks+physics_marks+chemistry_marks)/3;
-    System.out.print("Sam’s average mark in PCM is "+averageOfMarks);
-  }
-}
--- Level_1/Calculate_Volume.java
+++
@@ -1,8 +0,0 @@
-public class Calculate_Volume{
-  public static void main(String[] args){
-    int radius= 6378;
-    double volume_in_kilometers= ((4/3)*3.14*radius);
-    double volume_in_miles= volume_in_kilometers*1.6;
-    System.out.print("The volume of earth in cubic kilometers is "+volume_in_kilometers+" and cubic miles is "+volume_in_miles);
-  }
-}
