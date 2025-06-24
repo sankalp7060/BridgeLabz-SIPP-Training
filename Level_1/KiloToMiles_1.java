a1e36312d2ea4448b0d0e408157848c27254d930
Sankalp <129958359+sankalp7060@users.noreply.github.com>
2025-06-24 08:40:39 +0530

    Delete Level_1 directory
    [Sankalp] delete Level_1 directory

--- Level_1/KiloToMiles_1.java
+++
@@ -1,7 +0,0 @@
-public class KiloToMiles_1{
-  public static void main(String[] args){
-    double kilometers=10.8;
-    double miles=1.6*10.8;
-    System.out.print("The distance "+kilometers+" km in miles is "+miles);
-  }
-}
